2926f786c33c94d385afb6bd90647d0119c178dc
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-02-15 13:41:38 +0700

    implement sync

--- lib/Git/Bunch.pm
+++ lib/Git/Bunch.pm
@@ -6,6 +6,7 @@ use strict;
 use warnings;
 use Log::Any '$log';
 
+use Cwd ();
 use File::chdir;
 use File::Path qw(make_path);
 use String::ShellQuote;
@@ -71,12 +72,260 @@ sub check_bunch {
             $res{$repo} = [500, "Not a git repo (2)"];
         } else {
             $log->error("Can't figure out result of 'git status' ".
-                            "for repo `$repo`: exit=$exit, output=$output");
+                            "for repo $repo: exit=$exit, output=$output");
             $res{$repo} = [500, "Unknown (exit=$exit, output=$output)"];
         }
         $CWD = "..";
     }
     [200, "OK", \%res];
+}
+
+sub _mysystem {
+    $log->trace("system(): ".join(" ", @_));
+    system @_;
+}
+
+sub _myqx {
+    my $cmd = shift;
+    $log->trace("qx(): $cmd");
+    `$cmd`
+}
+
+sub _sync_repo {
+    my ($src, $dest, $repo, $opts) = @_;
+    my $exit;
+
+    my @src_branches;
+    my @dest_branches;
+    my %src_heads;  # last revisions for each branch
+    my %dest_heads; # last revisions for each branch
+
+    local $CWD = "$src/$repo";
+    @src_branches = map {(/^[* ] (.+)/, $1)[-1]} _myqx("LANG=C git branch");
+    $exit = $? & 255;
+    if ($exit) {
+        $log->error("Can't list branches on src repo $src/$repo: $?");
+        return [500, "Can't list source branches"];
+    }
+    $log->debugf("Source branches: %s", \@src_branches);
+
+    for my $branch (@src_branches) {
+        my $output = _myqx("LANG=C git log -1 '$branch'");
+        $exit = $? & 255;
+        if ($exit) {
+            $log->error("Can't find out head for branch $branch on src repo ".
+                            "$src/$repo: $?");
+            return [500, "Can't find out head for source branch $branch"];
+        }
+        $output =~ /commit (\S+)/ or do {
+            $log->error("Can't recognize git log output ".
+                            "(searching for commit XXX): $output");
+            return [500, "Can't recognize git log output on src: $output"];
+        };
+        $src_heads{$branch} = $1;
+    }
+    $log->debugf("Source branch heads: %s", \%src_heads);
+
+    $CWD = "$dest/$repo";
+    @dest_branches = map {(/^[* ] (.+)/, $1)[-1]} _myqx("LANG=C git branch");
+    if ($exit) {
+        $log->error("Can't list branches on dest repo $repo: $?");
+        return [500, "Can't list branches on dest: $?"];
+    }
+    $log->debugf("Dest branches: %s", \@dest_branches);
+    for my $branch (@dest_branches) {
+        my $output = _myqx("LANG=C git log -1 '$branch'");
+        $exit = $? & 255;
+        if ($exit) {
+            $log->error("Can't find out head for branch $branch on dest repo ".
+                            "$dest/$repo: $?");
+            return [500, "Can't find out head for dest branch $branch"];
+        }
+        $output =~ /commit (\S+)/ or do {
+            $log->error("Can't recognize git log output ".
+                            "(searching for commit XXX): $output");
+            return [500, "Can't recognize git log output on src: $output"];
+        };
+        $dest_heads{$branch} = $1;
+    }
+    $log->debugf("Dest branch heads: %s", \%dest_heads);
+
+    my $output;
+    my $lock_deleted;
+  BRANCH:
+    for my $branch (@src_branches) {
+        # XXX we should allow fetching tags only even if head is the same, but
+        # right now tags are not that important
+        if ($src_heads{$branch} && $dest_heads{$branch} &&
+                $src_heads{$branch} eq $dest_heads{$branch}) {
+            $log->debug("Skipping branch $branch because heads are the same");
+            next BRANCH;
+        }
+        if (0 && !$lock_deleted++) {
+            $log->debug("Deleting locks first ...");
+            unlink "$src/$repo/.git/index.lock";
+            unlink "$dest/$repo/.git/index.lock";
+        }
+        $log->info("Updating branch $branch ...") if @src_branches > 1;
+        $output = _myqx(
+            join("",
+                 "cd '$dest/$repo'; ",
+                 ($branch ~~ @dest_branches ? "":"git branch '$branch'; "),
+                 "git checkout '$branch' 2>/dev/null; ",
+                 "LANG=C git pull '$src/$repo' '$branch' 2>&1"
+             ));
+        $exit = $? & 255;
+        if ($exit == 0 && $output =~ /Already up-to-date/) {
+            $log->debug("Branch `$branch is up to date");
+            next BRANCH;
+        } elsif ($output =~ /^error: (.+)/m) {
+            $log->error("Can't successfully git pull branch $branch: $1");
+            return [500, "git pull branch $branch failed: $1"];
+        } elsif ($exit == 0 &&
+                     $output =~ /^Updating |^Merge made by recursive/m) {
+            $log->info("  Branch $branch updated") if @src_branches > 1;
+            $log->info("  Repo $repo updated"    ) if @src_branches == 1;
+        } else {
+            $log->error("Can't recognize 'git pull' output for ".
+                            "branch $branch: exit=$exit, output=$output");
+            return [500, "Can't recognize git pull output: $output"];
+        }
+        $log->debug("Result of 'git pull' for branch $branch: ".
+                        "exit=$exit, output=$output");
+
+        $output = _myqx("cd '$dest/$repo'; ".
+                            "LANG=C git fetch --tags '$src/$repo' 2>&1");
+        $exit = $? & 255;
+        if ($exit != 0) {
+            $log->debug("Can't successfully fetch --tags: ".
+                            "$output (exit=$exit)");
+            return [500, "git fetch --tags failed: $1"];
+        }
+    }
+
+    if ($opts->{delete_branch}) {
+        for my $branch (@dest_branches) {
+            next if $branch ~~ @src_branches;
+            next if $branch eq 'master'; # can't delete master branch
+            $log->info("Deleting branch $branch because it no longer exists ".
+                           "in src ...");
+            _mysystem("cd '$dest/$repo' && git checkout master 2>/dev/null && ".
+                          "git branch -D '$branch' 2>/dev/null");
+            if (($? & 255) != 0) {
+                $log->error("Can't successfully delete branch $branch: $?");
+            }
+        }
+    }
+
+    [200, "OK"];
+}
+
+$SUBS{sync_bunch} = {
+    summary       =>
+        'Synchronize bunch to another bunch',
+    description   => <<'_',
+
+For each git repository in the bunch, will perform a 'git pull' from the
+destination for each branch. If repository in destination doesn't exist, it will
+be rsync-ed first from source. When 'git pull' fails, will exit to let you fix
+the problem manually.
+
+For all other non-git repos, will simply synchronize by one-way rsync.
+
+_
+    required_args => [qw/source target/],
+    args          => {
+        source           => ['str*'   => {
+            summary      => 'Source bunch',
+            arg_pos      => 0,
+        }],
+        target           => ['str*'   => {
+            summary      => 'Destination bunch',
+            arg_pos      => 1,
+        }],
+        repos            => ['array'   => {
+            of           => 'str*',
+            summary      => 'Specific git repos to sync, if not specified '.
+                'all repos in the bunch will be processed',
+        }],
+        delete_branch    => ['bool'   => {
+            summary      => 'Whether to delete branches in dest repos '.
+                'not existing in source repos',
+            default      => 0,
+        }],
+    },
+    cmdline_suppress_output => 1,
+};
+sub sync_bunch {
+    my %args = @_;
+
+    # XXX schema
+    my $source        = $args{source} or return [400, "Please specify source"];
+    $source           =~ s!/+$!!;
+    (-d $source) or return [404, "Source doesn't exist"];
+    $source           = Cwd::abs_path($source);
+    my $target        = $args{target} or return [400, "Please specify target"];
+    $target           =~ s!/+$!!;
+    $target           = Cwd::abs_path($target);
+    my $wanted_repos  = $args{repos};
+    return [400, "repos must be an array"]
+        if defined($wanted_repos) && ref($wanted_repos) ne 'ARRAY';
+    my $delete_branch = $args{delete_branch} // 0;
+
+    my $cmd;
+
+    local $CWD = $source;
+    my @entries;
+    opendir my($d), "."; @entries = readdir($d);
+
+    unless (-d $target) {
+        $log->debugf("Creating target directory %s ...", $target);
+        make_path($target)
+            or return [500, "Can't create target directory $target: $!"];
+    }
+
+    $CWD = $target;
+    my %res;
+  ENTRY:
+    for my $e (@entries) {
+        next if $e eq '.' || $e eq '..';
+        my $is_repo = (-d "$source/$e") && (-d "$source/$e/.git");
+        if (!$is_repo) {
+            $log->info("Sync-ing non-git file/directory $e ...");
+            $cmd = "rsync -az --del --force ".shell_quote("$source/$e")." .";
+            _mysystem($cmd);
+            if ($?) {
+                $log->warn("Rsync failed, please check: $!");
+            }
+            next ENTRY;
+        }
+
+        if ($wanted_repos && !($e ~~ @$wanted_repos)) {
+            $log->debugf("Repo $e is not in wanted repos (%s), skipped",
+                         $wanted_repos);
+            next ENTRY;
+        }
+
+        if (!(-e $e)) {
+            $log->info("Copying repo $e ...");
+            $cmd = "rsync -az ".shell_quote("$source/$e")." .";
+            _mysystem($cmd);
+            if ($?) {
+                $log->warn("Rsync failed, please check: $!");
+            }
+            $log->info("  Repo $e copied");
+            next ENTRY;
+        } else {
+            $log->info("Sync-ing repo $e ...");
+            my $res = _sync_repo(
+                $source, $target, $e,
+                {delete_branch => $delete_branch},
+            );
+            $res{$e} = $res if $res->[0] != 200;
+        }
+    }
+
+    [200, "OK", {failed_syncs=>\%res}];
 }
 
 $SUBS{backup_bunch} = {
@@ -188,15 +437,16 @@ sub backup_bunch {
             "rsync -az ",
             ($log->is_trace() ? "-Pv" : ($log->is_debug() ? "-v" : "")), " ",
             "--include / ",
+            # dot-dirs are included recursively
+            "--include '/.??*' --include '/.??*/**' ",
+            # nondot-dirs are assumed git as repos, only .git/ copied from each
             "--include '/*' --include '/*/.git' --include '/*/.git/**' ",
-            #"--include '/.remote' --include '/.remote/**' ",
             "--exclude '*' ",
             "--del --force --delete-excluded ",
             shell_quote($source), "/ ",
             shell_quote($target), "/"
         );
-        $log->trace("system(): $cmd");
-        system $cmd;
+        _mysystem($cmd);
         return [500, "Backup did not succeed, please check: $!"] if $?;
     }
 
@@ -205,7 +455,7 @@ sub backup_bunch {
         local $CWD = $source;
         my $cmd = "( ls -laR | gzip -c > .ls-laR.gz ) && ".
             "cp .ls-laR.gz ".shell_quote($target);
-        $log->trace("system(): $cmd");
+        _mysystem($cmd);
         return [500, "Indexing did not succeed, please check: $!"] if $?;
     }
 
